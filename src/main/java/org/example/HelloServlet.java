512a70b4b9eca24f79d3461b448a80f2f081108c
Hicham Sbai <hicham.sbai@axa-winterthur.ch>
2016-10-03 14:42:57 +0200

    new world

--- src/main/java/org/example/HelloServlet.java
+++ src/main/java/org/example/HelloServlet.java
@@ -12,6 +12,6 @@ import javax.servlet.http.HttpServletResponse;
 public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().print("Hello world: " + InetAddress.getLocalHost());
+        resp.getWriter().print("Hello new world: " + InetAddress.getLocalHost());
     }
 }
